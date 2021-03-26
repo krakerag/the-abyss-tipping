ad2ff51d43769d1818d27d2c6c93254ebd85ae9f
Matthew Hallsworth <matthew.hallsworth@heffron.com.au>
2021-03-26 20:19:15 +1100

    Build working code to allow iteration and auto sorting of round results

--- src/components/Tipping.vue
+++ src/components/Tipping.vue
@@ -3,15 +3,17 @@
     <h1 class="font-black">Tipping Totals - 2021</h1>
     <table class="table-auto border mt-6">
       <thead>
-        <tr>
-          <th>Name</th>
-          <th>Total</th>
+        <tr class="bg-gray-200">
+          <th class="px-4">Name</th>
+          <th class="px-4">Total</th>
+          <th class="px-4 font-bold">Round Totals (starting from round 1)</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Matt</td>
-          <td>5</td>
+        <tr v-for="tipper in getSortedTips()" :key="tipper.name">
+          <td class="px-4">{{ tipper.name }}</td>
+          <td class="px-4">{{ tipper.total }}</td>
+          <td class="px-4">{{ tipper.tips.join() }}</td>
         </tr>
       </tbody>
     </table>
@@ -19,12 +21,37 @@
 </template>
 
 <script>
+import Tips from '@/tips.json';
+
 export default {
   name: 'Tipping',
-  props: {}
+  methods: {
+    getSortedTips() {
+      const tips = Tips.map((element) => {
+        element.total = element.tips.reduce((result, item) => {
+          return result + item;
+        }, 0);
+        return element;
+      });
+
+      tips.sort((a, b) => {
+        if (a.total < b.total) {
+          return 1;
+        }
+        if (a.total > b.total) {
+          return -1;
+        }
+        return 0;
+      });
+
+      return tips;
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  tbody tr:nth-child(odd) {
+    @apply bg-gray-100;
+  }
 </style>
