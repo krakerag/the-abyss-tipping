ad5c09486b84baf69dc291d328168048949ce42f
Matthew Hallsworth <matthew.hallsworth@heffron.com.au>
2021-03-26 16:19:32 +1100

    Add tailwind and add placeholder

---
+++ src/components/Tipping.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="pt-12 pl-12 w-auto">
+    <h1 class="font-black">Tipping Totals - 2021</h1>
+    <table class="table-auto border mt-6">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Matt</td>
+          <td>5</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tipping',
+  props: {}
+}
+</script>
+
+<style scoped>
+
+</style>
